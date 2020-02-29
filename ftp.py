2f541f2df1c9f9fb8e90f8b5af1c90ffdd2fa4a6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-02-29 12:58:57 +0800

    Fix Typo and organize

--- ftp.py
+++ ftp.py
@@ -3,8 +3,6 @@
 import socket
 
 port_list = [21, 22, 25, 80, 110, 443]
-port_open = True
-
 services = {
     'ftp': 21,
     'ssh' : 22,
@@ -17,20 +15,21 @@ def retBanner(ip, port):
         socket.setdefaulttimeout(2)
         s = socket.socket()
         s.connect((ip, port))
-        banner = s.recv(1024)
-        return banner
+        return s.recv(1024)
     except:
+        print('[-] no return for {}'.format(ip))
         return
 
 def main():
-    ip1 = '192.168.95.148'
-    ip2 = '192.168.95.149'
+    ip1 = '192.168.100.102'
+    ip2 = '192.168.100.17'
     port = 21
 
     banner1 = retBanner(ip1, port)
+    banner2 = retBanner(ip2, port)
+    print(banner1, banner2)
     if banner1: print("[+] {} : {}".format(ip1, banner1))
     if banner2: print("[+] {} : {}".format(ip2, banner2))
 
-
-if __name == '__main__':
+if __name__ == '__main__':
     main()
