0e0a6ae2d31ba8b145295adc3c4fd3abc4b73fd4
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-02-29 13:34:13 +0800

    Loop IP addresses and functions to scan whole subnet

--- ftp_scanner.py
+++ ftp_scanner.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 import socket
 
-port_list = [21, 22, 25, 80, 110, 443]
-services = {
-    'ftp': 21,
-    'ssh' : 22,
-    'smtp': 25,
-    'http': 80
-}
-
 def retBanner(ip, port):
     try: 
         socket.setdefaulttimeout(2)
@@ -17,7 +9,7 @@ def retBanner(ip, port):
         s.connect((ip, port))
         return s.recv(1024)
     except:
-        print('[-] no return for {}'.format(ip))
+        print('[-] no return for IP {} in  Port {}'.format(ip, port))
         return
 
 def checkVulns(banner):
@@ -34,19 +26,14 @@ def checkVulns(banner):
     return
 
 def main():
-    ip1 = '192.168.100.102'
-    ip2 = '192.168.100.17'
-    port = 21
-
-    banner1 = retBanner(ip1, port)
-    banner2 = retBanner(ip2, port)
-    print(banner1, banner2)
-    if banner1:
-        print("[+] {} : {}".format(ip1, banner1))
-        checkVulns(banner1)
-    if banner2:
-        print("[+] {} : {}".format(ip2, banner2))
-        checkVulns(banner2)
+    port_list = [21, 22, 25, 80, 110, 443]
+    for x in range(1, 255):
+        ip = '192.168.100.{}'.format(str(x))
+        for port in port_list:
+            banner = retBanner(ip, port)
+            if banner:
+                print("[+] {} : {}".format(ip, banner))
+                checkVulns(banner)
 
 if __name__ == '__main__':
     main()
