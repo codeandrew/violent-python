822fdf503687de25f5ef641c72aa704d38935636
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-03-03 00:13:08 +0800

    Organize main function

--- ftp_scanner.py
+++ ftp_scanner.py
@@ -4,18 +4,6 @@ import socket
 import sys
 import os
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-    if not os.path.isfile(filename):
-        print('[-] {} does not exist.'.format(filename))
-        exit(0)
-    if not os.access(filename, os.R_OK):
-        print('[-] {} access denied.'.format(filename))
-        exit(0)
-    print("[+] Reading Vulnerabilities From: {}".format(filename))
-
-
-
 def retBanner(ip, port):
     try: 
         socket.setdefaulttimeout(2)
@@ -26,21 +14,31 @@ def retBanner(ip, port):
         print('[-] no return for IP {} in  Port {}'.format(ip, port))
         return
 
-def checkVulns(banner):
-    f = open("vulnerable_banners.txt")
+def checkVulns(banner, filename):
+    f = open(filename, "r")
     for line in f.readlines():
         if line.strip('\n') in banner:
             print('[+] Server is vulnerable: {}'.format(banner.strip('\n')))
 
 def main():
-    port_list = [21, 22, 25, 80, 110, 443]
-    for x in range(1, 255):
-        ip = '192.168.100.{}'.format(str(x))
-        for port in port_list:
-            banner = retBanner(ip, port)
-            if banner:
-                print("[+] {} : {}".format(ip, banner))
-                checkVulns(banner)
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print('[-] {} does not exist.'.format(filename))
+            exit(0)
+        if not os.access(filename, os.R_OK):
+            print('[-] {} access denied.'.format(filename))
+            exit(0)
+        print("[+] Reading Vulnerabilities From: {}".format(filename))
+    else:
+        port_list = [21, 22, 25, 80, 110, 443]
+        for x in range(1, 255):
+            ip = '192.168.100.{}'.format(str(x))
+            for port in port_list:
+                banner = retBanner(ip, port)
+                if banner:
+                    print("[+] {} : {}".format(ip, banner))
+                    checkVulns(banner, filename)
 
 if __name__ == '__main__':
     main()
